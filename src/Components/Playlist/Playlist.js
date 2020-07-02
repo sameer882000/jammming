c3f0306c9cdf086a53cdaca23a099dc2f3166cef
Sameer <sameer.882000@gmail.com>
2020-07-02 16:26:19 +0530

    intial

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TrackList from '../TrackList/TrackList';
+import './Playlist.css';
+
+
+class Playlist extends React.Component{
+    render() {
+        return(
+            <div className="Playlist">
+  <input defaultValue={'New Playlist'} />
+  <TrackList />
+  <button className="Playlist-save">SAVE TO SPOTIFY</button>
+</div>
+        )
+    }
+}
+
+export default Playlist;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TrackList.css';
+
+class TrackList extends React.Component{
+    render() {
+        return(
+            <div className="TrackList">
+   
+</div>
+        )
+    }
+}
+
+
+export default TrackList;
