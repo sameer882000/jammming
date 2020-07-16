eb30edc1d4a31ca4500a760cd65b39aa77765266
Sameer <sameer.882000@gmail.com>
2020-07-16 11:07:43 +0530

    Update Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
-
 class Playlist extends React.Component{
     constructor(props){
         super(props);
